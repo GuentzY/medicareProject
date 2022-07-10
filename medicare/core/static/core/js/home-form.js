fe02dc6a025ab36a6e64fb7f4a9fb5c131f1e6c8
UentzY <jo.bonhomme@duocuc.cl>
2022-07-10 16:36:04 -0400

    Web base medicare 1.0

---
+++ medicare/core/static/core/js/home-form.js
@@ -0,0 +1,35 @@
+$(function(){
+    var $registerForm = $("#frmContact");
+    if($registerForm.length){
+        $registerForm.validate({
+            rules:{
+                txtNombreCompleto:{
+                    required: true
+                },
+                txtRut: {
+                    required: true
+                },
+                txtTelefono: {
+                    required: true
+                },
+                txtEmail: {
+                    required: false
+                }
+            },
+            messages:{
+                txtNombreCompleto:{
+                    required: 'Por favor, ingrese un nombre válido'
+                },
+                txtRut: {
+                    required: 'Ingrese su RUT'
+                },
+                txtTelefono: {
+                    required: 'Ingrese un numero de contacto'
+                },
+                txtEmail: {
+                    required: 'Ingrese un correo válido!'
+                }
+            }
+        })
+    }
+})
