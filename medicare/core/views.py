4b4557c9413b80b74de5730f3ec2e48a6f19ac40
UentzY <jo.bonhomme@duocuc.cl>
2022-07-11 09:52:21 -0400

    Llamada al modelo paciente

--- medicare/core/views.py
+++ medicare/core/views.py
@@ -1,7 +1,14 @@
 from urllib import request
 from django.shortcuts import render
+from .models import paciente
 
 # Create your views here.
+
 def home(request):
+    pacientes= paciente.objects.all()
     
-    return render(request, 'core/home.html')
+    datos = {
+        'pacientes' : pacientes
+    }
+    
+    return render(request, 'core,home.html', datos)
