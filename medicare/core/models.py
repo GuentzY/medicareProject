ed0419d8868eff0649253815d73c055b64ba8330
UentzY <jo.bonhomme@duocuc.cl>
2022-07-10 23:02:53 -0400

    Creación de las classes modelos

--- medicare/core/models.py
+++ medicare/core/models.py
@@ -1,3 +1,22 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+#Modelo para doctor
+class doctor(models.Model):
+    idDoctor= models.IntegerField(primary_key=True, verbose_name='Id del doctor')
+    nombreDoc= models.CharField(max_length=50, verbose_name='Nombre del doctor')
+    
+    
+    def __str__(self):
+        return self.nombreDoc
+
+
+#Modelo para paciente
+class paciente(models.Model):
+    nombrePac= models.CharField(max_length=25, verbose_name='Nombre del paciente')
+    apellidoPac= models.CharField(max_length=35, verbose_name='Apellido del paciente')
+    runPac= models.CharField(max_length=10, verbose_name='Rut del paciente')
+    telefonoPac= models.IntegerField(max_length=9, verbose_name='Teléfono del paciente')
+    correoPac= models.CharField(max_length=20, verbose_name='Correo del paciente')
